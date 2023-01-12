91697dd4aae605b59c5d1992dfafd2585ea69c7a
WildTooth <champenV@outlook.dk>
2023-01-12 21:07:25 +0100

    Created Plush.java

---
+++ src/main/java/com/github/wildtooth/plush/plush/Plush.java
@@ -0,0 +1,17 @@
+package com.github.wildtooth.plush.plush;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plush extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
