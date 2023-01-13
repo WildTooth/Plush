4558e67086c3b55a8a4f2c041d2101c21bf30b54
WildTooth <champenV@outlook.dk>
2023-01-13 13:01:33 +0100

    Added IllegalUseThreat

---
+++ src/main/java/com/github/wildtooth/plush/security/threat/IllegalUseThreat.java
@@ -0,0 +1,61 @@
+package com.github.wildtooth.plush.security.threat;
+
+import com.github.wildtooth.plush.util.ColorUtils;
+import org.bukkit.BanList;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class IllegalUseThreat extends Threat {
+
+    private final ItemStack item;
+    private final Player cause;
+
+    public IllegalUseThreat(String description,
+                            ThreatLevel threatLevel,
+                            ItemStack theItem,
+                            Player thePlayer) {
+        super(ThreatType.ILLEGAL_ITEM, description, threatLevel);
+        this.item = theItem;
+        this.cause = thePlayer;
+    }
+
+    @Override
+    public void execute() {
+        if (severity().equals(ThreatLevel.LOW)) return;
+
+        if (severity().equals(ThreatLevel.MEDIUM)) {
+            cause.kickPlayer(ColorUtils.getColored("&cYou have been kicked, use the right code!"));
+            return;
+        }
+
+        if (severity().equals(ThreatLevel.HIGH)) {
+            cause.getServer().getBanList(BanList.Type.NAME).addBan(ChatColor.stripColor(
+                            cause.getName()),
+                    ColorUtils.getColored(
+                            "&cYou have been banned, contact server-administration! &4&lError Code: &c&lWRONG_CODE"
+                    ),
+                    null,
+                    null);
+            cause.kickPlayer("&cYou have been banned, contact server-administration! &4&lError Code: &c&lWRONG_CODE");
+        }
+    }
+
+    public ItemStack getItem() {
+        return item;
+    }
+
+    public Player getCause() {
+        return cause;
+    }
+
+    /**
+     * Severity threat level.
+     *
+     * @return the threat level
+     */
+    public ThreatLevel severity() {
+        return getThreatLevel();
+    }
+}
+
