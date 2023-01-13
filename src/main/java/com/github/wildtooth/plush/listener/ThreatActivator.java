fab4f2d2a450d347d651124ad6bf572d65f92fe9
WildTooth <champenV@outlook.dk>
2023-01-13 20:54:13 +0100

    Added functionality and docs

--- src/main/java/com/github/wildtooth/plush/listener/ThreatActivator.java
+++ src/main/java/com/github/wildtooth/plush/listener/ThreatActivator.java
@@ -2,16 +2,42 @@ package com.github.wildtooth.plush.listener;
 
 import com.github.wildtooth.plush.Plush;
 import com.github.wildtooth.plush.event.item.IllegalItemUseEvent;
+import com.github.wildtooth.plush.security.threat.IllegalUseThreat;
+import com.github.wildtooth.plush.security.threat.ThreatLevel;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * The type Threat activator.
+ */
 public class ThreatActivator implements Listener {
 
+    private final Plush plugin;
+
+    /**
+     * Instantiates a new Threat activator.
+     *
+     * @param plugin the plugin
+     */
     public ThreatActivator(@NotNull Plush plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        this.plugin = plugin;
     }
 
+    /**
+     * On illegal item use.
+     *
+     * @param event the event
+     */
+    @EventHandler
     public void onIllegalItemUse(IllegalItemUseEvent event) {
+        IllegalUseThreat threat = new IllegalUseThreat(
+                "Illegal item use detected",
+                ThreatLevel.LOW,
+                event.getItem(),
+                event.getPlayer());
 
+        plugin.getThreatHandler().handle(threat);
     }
 }
