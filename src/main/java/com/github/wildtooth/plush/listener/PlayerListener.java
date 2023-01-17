920860c98d081328434ebc454ef2f9533c9abccb
WildTooth <champenV@outlook.dk>
2023-01-17 20:22:13 +0100

    Removed invalid import statements and empty lines

--- src/main/java/com/github/wildtooth/plush/listener/PlayerListener.java
+++ src/main/java/com/github/wildtooth/plush/listener/PlayerListener.java
@@ -5,10 +5,8 @@ import com.github.wildtooth.plush.auth.AuthHandler;
 import com.github.wildtooth.plush.event.code.WrongCodeEvent;
 import com.github.wildtooth.plush.event.item.IllegalItemUseEvent;
 import com.github.wildtooth.plush.manager.DataManager;
-import com.github.wildtooth.plush.security.threat.WrongCodeThreat;
 import com.warrenstrange.googleauth.GoogleAuthenticator;
 import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -26,9 +24,6 @@ import java.util.UUID;
  */
 public final class PlayerListener implements Listener {
 
-    private final DataManager dataManager;
-    private final AuthHandler authHandler;
-
     /**
      * Instantiates a new Player listener.
      *
@@ -36,63 +31,6 @@ public final class PlayerListener implements Listener {
      */
     public PlayerListener(@NotNull Plush plugin, @NotNull AuthHandler authHandler) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        this.authHandler = authHandler;
-        this.dataManager = plugin.getDataManager();
-    }
-
-    @EventHandler
-    public void onPlayerJoin(@NotNull PlayerJoinEvent event) {
-        final Player player = event.getPlayer();
-
-        if (!this.dataManager.hasCredentials(player)) {
-            GoogleAuthenticator gAuth = new GoogleAuthenticator();
-            GoogleAuthenticatorKey key = gAuth.createCredentials();
-
-            player.sendMessage("§7You must enter this code in the Google Authenticator App before leaving the server.");
-            player.sendMessage("§7Your §bGoogle Auth Code §7is §a" + key.getKey());
-
-            this.dataManager.addIfAbsent(player, key.getKey());
-        } else {
-            authHandler.handle(player);
-            player.sendMessage("§cPlease open the Google Authenticator App and provide the six digit code.");
-        }
-    }
-
-    @EventHandler
-    public void onPlayerQuit(@NotNull PlayerQuitEvent event) {
-        final UUID uuid = event.getPlayer().getUniqueId();
-        authHandler.handle(uuid);
-    }
-
-    @EventHandler
-    public void onChat(@NotNull AsyncPlayerChatEvent event) {
-        final Player player = event.getPlayer();
-        final String message = event.getMessage();
-
-        if (authHandler.isAuthLocked(player)) {
-            try {
-                Integer code = Integer.parseInt(message);
-                if (playerInputCode(player, code)) {
-                    authHandler.handle(player);
-                    player.sendMessage("§a*Access Granted* §bWelcome to the server!");
-                } else {
-                    player.sendMessage("§cIncorrect or expired code ** A code will only contain numbers **");
-                    WrongCodeEvent wrongCodeEvent = new WrongCodeEvent(code, player);
-                    player.getServer().getPluginManager().callEvent(wrongCodeEvent);
-                }
-            } catch (Exception e) {
-                player.sendMessage("§cIncorrect or expired code ** A code will only contain numbers **");
-            }
-            event.setCancelled(true);
-        }
-    }
-
-    private boolean playerInputCode(Player player, int code) {
-        final String secretKey = this.dataManager.getCredentials(player);
-
-        GoogleAuthenticator gAuth = new GoogleAuthenticator();
-
-        return gAuth.authorize(secretKey, code);
     }
 
     /**
